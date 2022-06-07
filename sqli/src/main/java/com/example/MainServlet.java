51e08244a3c622900f1a6643060f3bb73e9f6982
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-06-07 21:59:17 +0300

    Add files via upload

--- sqli/src/main/java/com/example/MainServlet.java
+++ sqli/src/main/java/com/example/MainServlet.java
@@ -16,59 +16,41 @@ import java.util.regex.Matcher;
 
 @WebServlet("/hello")
 public class MainServlet extends HttpServlet{
-    // declare array of commands to check against, can add to this easily
     public static final String stringsToCheck[] = { "select", "drop", "from",
             "exec", "exists", "update", "delete", "insert", "cast", "http",
             "sql", "null", "like", "mysql", "()", "information_schema",
             "sleep", "version", "join", "declare", "having", "signed", "alter",
             "union", "where", "create", "shutdown", "grant", "privileges" };
 
-    // for reference, regex metachars that need escaped <([{\^-=$!|]})?*+.>
-
-    // instantiate each com.example.RegexObj with the expression and a plain english
-    // description
-
-    // /* and */
     public static RegexObj regex1 = new RegexObj("(/\\*).*(\\*/)",
             "Found /* and */");
 
-    // -- at the end
     public static RegexObj regex2 = new RegexObj("(--.*)$", "-- at end of sql");
 
-    // ; and at least one " or '
     public static RegexObj regex3 = new RegexObj(";+\"+\'",
             "One or more ; and at least one \" or \'");
 
-    // two or more "
     public static RegexObj regex4 = new RegexObj("\"{2,}+", "Two or more \"");
 
-    // two or more '
     public static RegexObj regex5 = new RegexObj("\'{2,}+", "Two or more \'");
 
-    // anydigit=anydigit
     public static RegexObj regex6 = new RegexObj("\\d=\\d", "anydigit=anydigit");
 
-    // two or more white spaces in a row
     public static RegexObj regex7 = new RegexObj("(\\s\\s)+",
             "two or more white spaces in a row");
 
-    // # at the end
     public static RegexObj regex8 = new RegexObj("(#.*)$", "# at end of sql");
 
-    // two or more %
     public static RegexObj regex9 = new RegexObj("%{2,}+",
             "Two or more \\% signs");
 
-    // admin and one of [; ' " =] before or after admin
     public static RegexObj regex10 = new RegexObj(
             "([;\'\"\\=]+.*(admin.*))|((admin.*).*[;\'\"\\=]+)",
             "admin (and variations like administrator) and one of [; ' \" =] before or after admin");
 
-    // ASCII in hex
     public static RegexObj regex11 = new RegexObj("%+[0-7]+[0-9|A-F]+",
             "ASCII Hex");
 
-    // declare array to hold each regex, can add to this easily
     public static final RegexObj regexes[] = { regex1, regex2, regex3, regex4,
             regex5, regex6, regex7, regex8, regex9, regex10, regex11 };
 
@@ -86,7 +68,6 @@ public class MainServlet extends HttpServlet{
             printWriter.write("Hello, Anonymous" + "<br>");
             printWriter.write("Hello, " +  line.replace(" ", "+") + "<br>");
         }
-       //  /*
 
         else if (username.equals(line)){
             int prediction;
@@ -101,10 +82,8 @@ public class MainServlet extends HttpServlet{
             boolean flag2 = false;
             boolean flag3 = false;
 
-            // get the label and store it
             char label = line.charAt(0);
 
-            // increment the true totals accordingly
             if (label == '0') {
                 condNeg++;
             } else if (label == '1') {
@@ -112,11 +91,8 @@ public class MainServlet extends HttpServlet{
                 flag1 = true;
             }
 
-            // get the sql string
             String sqlString = line.substring(2);
 
-            // if true is returned, then classify as malware, otherwise
-            // benign
             if (sqlHandler(sqlString)) {
                 prediction = 1;
                 if (Character.getNumericValue(label) == prediction) {
@@ -175,11 +151,10 @@ public class MainServlet extends HttpServlet{
         try{
             FileReader fileReader = new FileReader(f);
 
-            // create new buffered reader
             BufferedReader br = new BufferedReader(fileReader);
             String read;
             while ((read = br.readLine()) != null){
-                wordsList.add(read);               // читаем все слова из файла и добавляем в коллекцию
+                wordsList.add(read);
             }
             System.out.println(wordsList);
         }catch(FileNotFoundException e){
@@ -188,21 +163,18 @@ public class MainServlet extends HttpServlet{
             System.out.println("I/O error.");
         }
 
-        return wordsList.get(4);
+        return wordsList.get(33);
     }
 
 
     public static boolean sqlHandler(String sqlString) {
 
-        // use two more bools for returns from sqlStringChecker and sqlRegexChecker
         boolean pass1 = false;
         boolean pass2 = false;
 
-        // call both, pass in the string
         pass1 = sqlStringChecker(sqlString);
         pass2 = sqlRegexChecker(sqlString);
 
-        // if either checker is true return true otherwise return false
         if (pass1 || pass2) {
             return true;
         } else {
@@ -217,11 +189,8 @@ public class MainServlet extends HttpServlet{
 
         System.out.println("\nRunning SQL String Checker");
 
-        // convert to lower case to handle obfuscation with mixed upper and
-        // lower case
         sqlToCheck = sqlToCheck.toLowerCase();
 
-        // for each string in stringsToCheck
         for (String command : stringsToCheck) {
 
             if (sqlToCheck.contains(command)) {
@@ -248,22 +217,15 @@ public class MainServlet extends HttpServlet{
 
         System.out.println("\nRunning SQL Regex Checker");
 
-        // bool for each regex
         boolean pass = false;
-        // bool to return overall
         boolean overall = false;
 
         Matcher matcher;
 
-        // convert to lower case to handle obfuscation with mixed upper and
-        // lower case
+
         sqlToCheck = sqlToCheck.toLowerCase();
 
-        // regex checking
         for (RegexObj regex : regexes) {
-
-            // check sqlToCheck vs regex, if pattern returns i.e. regex returns
-            // true
 
             matcher = regex.getRegexPattern().matcher(sqlToCheck);
 
@@ -282,8 +244,6 @@ public class MainServlet extends HttpServlet{
 
             }
 
-            // if a regex returns true for the first time (i.e. overall is still
-            // false), then make overall true
             if ((pass) && (!overall)) {
                 overall = true;
             }
